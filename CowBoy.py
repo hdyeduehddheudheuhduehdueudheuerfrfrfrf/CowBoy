367c4d355e24d8c63498c767ade9067e18ccf1b9
mrxD1MOD <99124744+mrxD1MOD@users.noreply.github.com>
2022-02-09 15:32:58 +0300

    Update CowBoy.py

--- CowBoy.py
+++ CowBoy.py
@@ -280,7 +280,7 @@ else:
 		usage()
 		sys.exit()
 	else:
-		print "-- HULK Attack Started --"
+		print "-- CowBoy Attack Started --"
 		if len(sys.argv)== 3:
 			if sys.argv[2]=="safe":
 				set_safe()
